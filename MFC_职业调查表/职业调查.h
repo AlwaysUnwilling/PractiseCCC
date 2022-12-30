bd5ed9c35e623c76e99c021c530d16421ff5026f
AlwaysUnwilling <747266971@qq.com>
2022-12-30 23:16:38 +0800

    MFC_职业调查表

---
+++ MFC_职业调查表/职业调查.h
@@ -0,0 +1,32 @@
+﻿
+// 职业调查.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// C职业调查App:
+// 有关此类的实现，请参阅 职业调查.cpp
+//
+
+class C职业调查App : public CWinApp
+{
+public:
+	C职业调查App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern C职业调查App theApp;
